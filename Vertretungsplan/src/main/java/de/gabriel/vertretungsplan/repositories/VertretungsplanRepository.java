d1768ebd50531928c1502ce83cc706a094e542eb
gabriel-java-github <gabriel.haberlach@gmail.com>
2022-05-29 15:32:55 +0200

    Initial Commit

--- Vertretungsplan/src/main/java/de/gabriel/vertretungsplan/repositories/VertretungsplanRepository.java
+++
@@ -1,9 +0,0 @@
-package de.gabriel.vertretungsplan.repositories;
-
-import de.gabriel.vertretungsplan.models.Vertretungsplan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface VertretungsplanRepository extends JpaRepository<Vertretungsplan, Integer> {
-
-}
